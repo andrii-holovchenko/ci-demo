8b0c0cce03e6a921360c8aa96fea08d823084860
Andrii Holovchenko <andriih@nvidia.com>
2021-01-21 22:05:28 +0200

    test2
    Signed-off-by: Andrii Holovchenko <andriih@nvidia.com>

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -322,21 +322,25 @@ def check_skip_stage(image, config, title, oneStep, axis) {
     }
 
     //def customSelList = toStringMap(oneStep.get("containerSelector"))
-    def customSelList = oneStep.get("containerSelector")
 
-    if (customSelList.size() > 0) {
-        customSelList.each {
-            def customSel = toStringMap(it)
+    if (oneStep['containerSelector'] && axis['category'] != 'tool') {
+        customSelList = oneStep['containerSelector']
+        println(customSelList)
+        println(customSelList.size())
+    
+        if (customSelList.size() > 0) {
+            customSelList.each {
+                def customSel = toStringMap(it)
 
-            // no match - skip
-            if (!matchMapEntry([customSel], axis)) {
-                config.logger.trace(2, "Step '" + title + "' skipped as no match by containerSelector=" + customSel + " for image with axis=" + axis)
-                return true
+                // no match - skip
+                if (!matchMapEntry([customSel], axis)) {
+                    config.logger.trace(2, "Step '" + title + "' skipped as no match by containerSelector=" + customSel + " for image with axis=" + axis)
+                    return true
+                }
+    
+                config.logger.debug("Step '" + title + "' will use axis=" + axis)
             }
-
-            config.logger.debug("Step '" + title + "' will use axis=" + axis)
         }
-
     } else if (axis['category'] == 'tool') {
             config.logger.trace(2, "Step '" + title + "' skipped for image category=tool")
             return true
